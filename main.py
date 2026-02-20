e7acd4a155b1c39cb597214fdbcfd014cfd9de36
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-02-20 16:09:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+# 1
+def salomlashuv():
+    print("Salom, dunyo!")
+
+salomlashuv()
+
+# 2
+def raqam_korsatsh():
+    print(100)
+
+raqam_korsatsh()
+
+# 3
+def motivatsiya():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+motivatsiya()
+
+# 4
+def takror():
+    print("Python" * 3)
+
+takror()
+
+
+# 5
+def hisob():
+    print(5 + 7)
+
+hisob()
+
+# 6
+
+def tong():
+    print("Hozir tong")
+
+tong()
+
+# 7
+def fikr():
+    print("O‘rganish - kelajak!")
+
+fikr()
+
+# 8
+def xabar():
+    print("Dastur ishga tushdi!")
+
+xabar()
+
+# 9
+def salom():
+    print("bugun yaxshi kun")
+
+salom()
+
+# 10
+def odiy():
+    print("xush kelibsiz")
+    
+odiy()
